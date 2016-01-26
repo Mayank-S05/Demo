2a697a1001c41a7929a79911959a4152b17b0241
boom <mayank.singh0597@gmail.com>
2016-01-26 15:00:54 +0530

    splash screen

---
+++ app/src/main/java/com/example/android/demo/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent mainActivityIntent = new Intent("com.example.android.demo.MAINACTIVITY");
+                    startActivity(mainActivityIntent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
